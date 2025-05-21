338d2bcc9de60dfbf042a1f6a2fc9721f696f931
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-05-21 12:13:44 +0200

    feat: add db connector sigleton

--- electricityBusiness/src/main/java/com/humanbooster/App.java
+++ electricityBusiness/src/main/java/com/humanbooster/App.java
@@ -1,10 +1,13 @@
 package com.humanbooster;
 
+import com.humanbooster.dao.Connector;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Connector dbConnector = Connector.getInstance();
     }
 }
